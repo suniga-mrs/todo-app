5c32a749431df6bea69daf9c3ab60a41cc43d9a6
Michelle Suniga <suniga.mrs@gmail.com>
2019-01-07 22:26:33 +0800

    To-do List App

--- app/partials/header.php
+++ app/partials/header.php
@@ -8,7 +8,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-none=no">
-	<title>To-Do App</title>
+	<title>📝 To-Do App</title>
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
